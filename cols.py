6ff3a18f07fdb685386c92f9cca355820757ff2d
makurell <makureoll@gmail.com>
2018-08-24 16:04:31 +0100

    work on processing + impl get_path

--- cols.py
+++ cols.py
@@ -1,4 +1,6 @@
 import re
+import os
+import time
 from io import StringIO
 
 #utils methods
@@ -14,12 +16,14 @@ def get_parts(s,skip_start=True):
     return ret
 
 class ColItem:
-    def __init__(self):
+    def __init__(self,parent):
         self.parts=[]
+        self.parent=parent
 
     def parse(self,raw):
         self.parts = get_parts(raw,skip_start=False)
 
+    #region serialisation
     def to_string(self):
         ret = ""
 
@@ -38,14 +42,16 @@ class ColItem:
 
     def __repr__(self):
         return self.to_string()
+    #endregion
 
 
 class ColSection:
-    def __init__(self):
+    def __init__(self,parent):
         self.items=[]
         self.parts=[]
         self.sections=[]
         self.depth=0
+        self.parent=parent
         pass
 
     def parse(self, raw):
@@ -68,7 +74,7 @@ class ColSection:
                             continue
                         if (get_level(innerline) <= start_level or len(innerline) <= 0) and len(buf)>0:
                             # flush buf
-                            sec = ColSection()
+                            sec = ColSection(self)
                             sec.parse(buf)
                             self.sections.append(sec)
 
@@ -79,9 +85,30 @@ class ColSection:
                             buf += innerline
                 else:
                     # ColItem
-                    item = ColItem()
+                    item = ColItem(self)
                     item.parse(line)
                     self.items.append(item)
+
+    #region serialisation
+    def get_name(self,name_version):
+        return re.sub("\([\s\S]*\)","",self.parts[name_version]).strip().replace(" ","-")
+
+    def get_path(self, name_version=1):
+        path_list = [self.get_name(name_version=name_version)]
+
+        parent=self.parent
+        while parent is not None:
+            try:
+                path_list.append(parent.get_name(name_version=name_version))
+            except AttributeError:
+                break
+            parent=parent.parent
+            pass
+
+        ret=''
+        for path_part in reversed(path_list):
+            ret+=path_part+'/'
+        return ret
 
     def to_string(self,indent="\t"):
         ind=""
@@ -112,6 +139,7 @@ class ColSection:
 
     def __repr__(self):
         return self.to_string()
+    #endregion
 
 class ColFile:
     def __init__(self,path):
@@ -120,10 +148,14 @@ class ColFile:
         self.meta = ""
         self.path = path
         self.depth=0
+        self.parent=None
 
-    def parse(self):
-        with open(self.path, 'r') as f:
-            self.raw=f.read()
+    def parse(self,raw=None):
+        if raw is None:
+            with open(self.path, 'r') as f:
+                self.raw=f.read()
+        else:
+            self.raw=raw
 
         started=False
         with StringIO(self.raw) as r:
@@ -148,7 +180,7 @@ class ColFile:
                                 continue
                             if (get_level(innerline) <= start_level or len(innerline) <= 0) and len(buf) > 0:
                                 #flush buf
-                                sec=ColSection()
+                                sec=ColSection(self)
                                 sec.parse(buf)
                                 self.sections.append(sec)
 
@@ -157,6 +189,45 @@ class ColFile:
                                 break
                             else:
                                 buf+=innerline
+
+    def build(self):
+        self.process()
+        self.render()
+
+    def process(self):
+        #make proc file
+        proc=ColFile(path=os.path.splitext(self.path)[0]+'.proc.col')
+        for section in self.sections:
+            proc.sections.append(ColFile.__process_helper(section))
+        proc.write()
+
+    @staticmethod
+    def __process_helper(proc: ColSection):
+        time.sleep(0.5)
+        print(proc.get_path())
+        section_list = []
+        section_list.extend(proc.sections)
+        proc.sections.clear()
+        for section in section_list:
+            proc.sections.append(ColFile.__process_helper(section))
+        proc=ColFile.__process_items(proc)
+        return proc
+
+    @staticmethod
+    def __process_items(proc: ColSection):
+        item_list = []
+        item_list.extend(proc.items)
+        proc.items.clear()
+        for item in item_list:
+            proc.items.append(item)
+        return proc
+
+    def render(self):
+        pass
+
+    #region serialisation
+    def get_path(self):
+        return ''
 
     def to_string(self,indent="\t"):
         ind=""
@@ -175,6 +246,10 @@ class ColFile:
         ret+=self.to_string(indent=indent)
         return ret
 
+    def write(self):
+        with open(self.path, 'w') as f:
+            f.write(self.serialise())
+
     def __str__(self):
         return self.to_string()
 
@@ -183,8 +258,10 @@ class ColFile:
 
     def __repr__(self):
         return self.to_string()
+    #endregion
 
 if __name__=="__main__":
     cf = ColFile("data.col")
     cf.parse()
-    print(cf.serialise())
+    #print(cf.serialise())
+    cf.build()
