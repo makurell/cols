b745fedd5065b6dd3e311dc0f7beb95a2e2a45c4
makurell <makureoll@gmail.com>
2018-08-26 13:45:39 +0100

    add copying unittest

--- coltests.py
+++ coltests.py
@@ -5,7 +5,7 @@ import builders
 from cols import run
 
 class TestRendering(unittest.TestCase):
-    def test_nochange(self):
+    def test_1_nochange(self):
         run("---\n"
             "- a - anime\n"
             "   - 1 - 1\n"
@@ -15,7 +15,7 @@ class TestRendering(unittest.TestCase):
             "       https://upload.wikimedia.org/wikipedia/commons/4/43/Chara04.png moe\n",False)
         self.assertTrue(os.path.isfile('cols/a/2/moe.jpeg'),"file exists")
 
-    def test_move(self):
+    def test_2_move(self):
         # move
         run("---\n"
             "- a - anime\n"
@@ -37,20 +37,60 @@ class TestRendering(unittest.TestCase):
         self.assertTrue(os.path.isfile('cols/a/2/moe.jpeg'), "dest file exists")
         self.assertFalse(os.path.isfile('cols/a/1/a/moe.jpeg'), "src file removed")
 
-    # def test_copy(self):
-    #     # copy
-    #     run("---\n"
-    #         "- a - anime\n"
-    #         "   - 1 - 1\n"
-    #         "       - a\n"
-    #         "           https://upload.wikimedia.org/wikipedia/commons/4/43/Chara04.png moe\n"
-    #         "   - 2 - 2\n"
-    #         "       https://upload.wikimedia.org/wikipedia/commons/4/43/Chara04.png moe\n", False)
-    #     self.assertTrue(os.path.isfile('cols/a/1/a/moe.jpeg'), "dest file exists")
-    #     self.assertTrue(os.path.isfile('cols/a/2/moe.jpeg'), "src file exists")
+    def test_3_copy(self):
+        # copy
+        run("---\n"
+            "- a - anime\n"
+            "   - 1 - 1\n"
+            "       - a\n"
+            "           https://upload.wikimedia.org/wikipedia/commons/4/43/Chara04.png moe\n"
+            "   - 2 - 2\n"
+            "       https://upload.wikimedia.org/wikipedia/commons/4/43/Chara04.png moe\n", False)
+        self.assertTrue(os.path.isfile('cols/a/1/a/moe.jpeg'), "dest file exists")
+        self.assertTrue(os.path.isfile('cols/a/2/moe.jpeg'), "src file exists")
+        # delete top
+        run("---\n"
+            "- a - anime\n"
+            "   - 1 - 1\n"
+            "       - a\n"
+            "           \n"
+            "   - 2 - 2\n"
+            "       https://upload.wikimedia.org/wikipedia/commons/4/43/Chara04.png moe\n", False)
+        self.assertFalse(os.path.isfile('cols/a/1/a/moe.jpeg'), "top file removed")
+        self.assertTrue(os.path.isfile('cols/a/2/moe.jpeg'), "bottom file exists")
+        # copy again
+        run("---\n"
+            "- a - anime\n"
+            "   - 1 - 1\n"
+            "       - a\n"
+            "           https://upload.wikimedia.org/wikipedia/commons/4/43/Chara04.png moe\n"
+            "   - 2 - 2\n"
+            "       https://upload.wikimedia.org/wikipedia/commons/4/43/Chara04.png moe\n", False)
+        self.assertTrue(os.path.isfile('cols/a/1/a/moe.jpeg'), "dest file exists")
+        self.assertTrue(os.path.isfile('cols/a/2/moe.jpeg'), "src file exists")
+        # delete bottom
+        run("---\n"
+            "- a - anime\n"
+            "   - 1 - 1\n"
+            "       - a\n"
+            "           https://upload.wikimedia.org/wikipedia/commons/4/43/Chara04.png moe\n"
+            "   - 2 - 2\n"
+            "       \n", False)
+        self.assertTrue(os.path.isfile('cols/a/1/a/moe.jpeg'), "top file exists")
+        self.assertFalse(os.path.isfile('cols/a/2/moe.jpeg'), "bottom file removed")
+        # move top to bottom (reset)
+        run("---\n"
+            "- a - anime\n"
+            "   - 1 - 1\n"
+            "       - a\n"
+            "           \n"
+            "   - 2 - 2\n"
+            "       https://upload.wikimedia.org/wikipedia/commons/4/43/Chara04.png moe\n", False)
+        self.assertFalse(os.path.isfile('cols/a/1/a/moe.jpeg'), "top file removed")
+        self.assertTrue(os.path.isfile('cols/a/2/moe.jpeg'), "bottom file exists")
 
 def setup_testing():
-    print('Setting up...')
+    # print('Setting up...')
     if os.path.exists('cols'):
         shutil.rmtree('cols',ignore_errors=True)
     if os.path.isfile('locs.json'):
@@ -61,9 +101,12 @@ def setup_testing():
         "       - a\n"
         "           \n"
         "   - 2 - 2\n"
-        "       https://upload.wikimedia.org/wikipedia/commons/4/43/Chara04.png moe\n")
-    print('[READY]')
+        "       https://upload.wikimedia.org/wikipedia/commons/4/43/Chara04.png moe\n",False)
+    # print('[READY]')
 
 if __name__=='__main__':
+    import cols
+    cols.DEBUG = False
     setup_testing()
-    unittest.main()
+
+    unittest.main(verbosity=2)
